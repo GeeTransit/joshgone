47cd5a8fdde49f36ce50071c1f9bd45a5aa92660
George Zhang <geetransit@gmail.com>
2021-01-19 21:10:39 -0500

    Make bot remove josh emojis

--- joshgone.py
+++ joshgone.py
@@ -2,6 +2,7 @@ import os
 import discord
 
 client = discord.Client()
+running = False
 
 @client.event
 async def on_ready():
@@ -9,9 +10,27 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global running
     if message.author == client.user:
         return
     if message.content.startswith("%joshgone"):
-        await message.channel.send("lmao boi this dont work just yet")
+        args = message.content.split()
+        if len(args) == 1:
+            await message.channel.send(f"JoshGone is currently {'running' if running else 'not running'}")
+            return
+        if len(args) == 2:
+            if args[1] == "true":
+                running = True
+            elif args[1] == "false":
+                running = False
+        return
+
+@client.event
+async def on_reaction_add(reaction, user):
+    global running
+    if user == client.user:
+        return
+    if running and reaction.emoji.name == "josh":
+        await reaction.remove(user)
 
 client.run(os.environ["JOSHGONE_TOKEN"])
