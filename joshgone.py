d3e06e9528404767aeb7909542a48074f1aa1d36
George Zhang <geetransit@gmail.com>
2021-03-28 22:55:16 -0400

    Add thicc to extensions to load

--- joshgone.py
+++ joshgone.py
@@ -7,7 +7,7 @@ import discord
 from discord.ext import commands
 
 # These extensions are loaded automatically on startup
-LOAD_ON_STARTUP = ["admin", "censor", "chant", "music", "database"]
+LOAD_ON_STARTUP = ["admin", "censor", "chant", "music", "database", "thicc"]
 
 # We need intents to resolve a name to a Member object
 intents = discord.Intents.default()
