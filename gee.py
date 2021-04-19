754cd8e15eeadbe1d0bf7b746ef0f3ced55199c2
George Zhang <geetransit@gmail.com>
2021-04-19 06:54:13 -0400

    Add random number generator to gee command

--- gee.py
+++ gee.py
@@ -58,9 +58,39 @@ class Gee(commands.Cog):
     async def gee(self, ctx, *, arg=None):
         """Reply with something GeeTransit would say"""
         if arg is None:
+            arg = ""
+        splitted = arg.split()
+
+        # Reply with a response if empty
+        if len(splitted) == 0:
             await ctx.send(random.choice(self.replies))
-        else:
-            await ctx.send(random.choice(self.replies_question))
+            return
+
+        # Pick random number given a bound (0 to a)
+        if len(splitted) == 1:
+            try:
+                a = int(splitted[0])
+            except ValueError:
+                pass
+            else:
+                x = random.randint(*sorted((0, a)))
+                await ctx.send(f"{x}")
+                return
+
+        # Pick random number given two bounds (a to b)
+        if len(splitted) == 2:
+            try:
+                a = int(splitted[0])
+                b = int(splitted[1])
+            except ValueError:
+                pass
+            else:
+                x = random.randint(*sorted((a, b)))
+                await ctx.send(f"{x}")
+                return
+
+        # Reply with yes or no to a question
+        await ctx.send(random.choice(self.replies_question))
 
 def setup(bot):
     bot.add_cog(Gee(bot))
