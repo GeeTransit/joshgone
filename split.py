dd9061ede08f95bc43e9597e0db65d40a8374c70
George Zhang <geetransit@gmail.com>
2021-04-23 18:03:18 -0400

    Alias lines to split and improve command

--- split.py
+++ split.py
@@ -10,18 +10,21 @@ class Split(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="split")
-    async def _split(self, ctx, delay: typing.Optional[float] = 0.5, *, lines):
-        """Splits and sends the argument's lines as separate messages
-
-        Used to send multiple commands from a single command when self
-        accept is on.
+    @commands.command(name="lines", aliases=["split"])
+    async def _lines(self, ctx, delay: typing.Optional[float] = 0.5, *, lines):
+        r"""Splits and sends the argument's lines as separate messages
 
         `delay` specifies the number of seconds between messages
+
+        Usage:
+            %lines a \n b           ->  sends a, b
+            %lines \n a \n \n b     ->  sends a, b
         """
         if not math.isfinite(delay):
             raise ValueError(f"{delay!r} is not finite")
         for line in lines.splitlines():
+            if not line or line.isspace():
+                continue
             await ctx.send(line)
             await asyncio.sleep(delay)
 
