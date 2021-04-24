f510c8d9cbf0e4c37adbbe229fae83c7def1a081
George Zhang <geetransit@gmail.com>
2021-04-23 22:39:57 -0400

    Allow sorting numbers

--- sort.py
+++ sort.py
@@ -13,8 +13,18 @@ class Sort(commands.Cog):
         Usage:
             %sort c b a     ->  sends a b c
             %sort a b c     ->  sends a b c
+            %sort 3 2 1     ->  sends 1 2 3
+            %sort 1 2 3     ->  sends 1 2 3
         """
-        await ctx.send(" ".join(sorted(args)))
+        # If all arguments can be ints, convert them
+        try:
+            args = [int(arg) for arg in args]
+        except ValueError:
+            try:
+                args = [float(arg) for arg in args]
+            except ValueError:
+                pass
+        await ctx.send(" ".join(map(str, sorted(args))))
 
 def setup(bot):
     bot.add_cog(Sort(bot))
