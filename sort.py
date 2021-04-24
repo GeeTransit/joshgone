0ab6bb428db13da89e1edd8b5d90a821fb86bd18
George Zhang <geetransit@gmail.com>
2021-04-24 08:37:42 -0400

    Preserve sort arguments' signs

--- sort.py
+++ sort.py
@@ -16,15 +16,144 @@ class Sort(commands.Cog):
             %sort 3 2 1     ->  sends 1 2 3
             %sort 1 2 3     ->  sends 1 2 3
         """
-        # If all arguments can be ints, convert them
         try:
-            args = [int(arg) for arg in args]
+            # If all arguments can be ints, convert them
+            result = sorted(args, key=int)
         except ValueError:
             try:
-                args = [float(arg) for arg in args]
+                # If all arguments can be floats, convert them
+                result = sorted(args, key=float)
             except ValueError:
-                pass
-        await ctx.send(" ".join(map(str, sorted(args))))
+                # Otherwise, just use lexicographical order
+                result = sorted(args)
+        await ctx.send(" ".join(result))
 
 def setup(bot):
     bot.add_cog(Sort(bot))
+> ** test **
+%h1 g-add-next
+
+[CODE]
+%chants add g-bold
+    %lines 3
+        - **
+        %get 1.2 5 1.2
+        %h1 g-bold-next
+
+[DOCS]
+%chants add g-bold-man
+```
+Usage:
+    > text
+    > %lines %h1 g-bold
+    ...
+    > ** text **
+    > %h1 g-bold-next
+```
+
+
+
+[RUNNING]
+> 5 6
+%lines %h1 a-plus-b
+%h1 a-plus-b
+%lines 3 (%chants update g-add-next %h1 a-plus-b-1, %get 6, %lines %h1 g-add)
+%chants update g-add-next %h1 a-plus-b-1
+> Updated chant g-add-next
+%get 6
+> 5 6
+%lines %h1 g-add
+...
+> 11
+> %h1 g-add-next
+%h1 a-plus-b-1
+%lines 3 (%chants remove g-bold-next, %get 6, %lines %h1 g-bold)
+%chants remove g-bold-next
+> Removed chant g-bold-next
+%get 6
+> 11
+%lines %h1 g-bold
+...
+> ** 11 **
+> %h1 g-bold-next
+%h1 a-plus-b-2
+%get 3
+> ** 11 **
+
+[CODE]
+%chants add a-plus-b
+    %lines 3
+        %chants update g-add-next %h1 a-plus-b-1
+        %get 6
+        %lines %h1 g-add
+%chants add a-plus-b-1
+    %lines 3
+        %chants update g-bold-next %h1 a-plus-b-2
+        %get 6
+        %lines %h1 g-bold
+%chants add a-plus-b-2
+    %get 3
+
+[DOCS]
+%chants add a-plus-b-man
+```
+Usage:
+    > 1 2
+    > %lines %h1 a-plus-b
+    ...
+    > ** 3 **
+```
+
+
+
+[RUNNING]
+> 5 6
+%lines %h1 g-min
+%h1 a-plus-b
+%lines 3 (%chants update g-add-next %h1 a-plus-b-1, %get 6, %lines %h1 g-add)
+%chants update g-add-next %h1 a-plus-b-1
+> Updated chant g-add-next
+%get 6
+> 5 6
+%lines %h1 g-add
+...
+> 11
+> %h1 g-add-next
+%h1 a-plus-b-1
+%lines 3 (%chants remove g-bold-next, %get 6, %lines %h1 g-bold)
+%chants remove g-bold-next
+> Removed chant g-bold-next
+%get 6
+> 11
+%lines %h1 g-bold
+...
+> ** 11 **
+> %h1 g-bold-next
+%h1 a-plus-b-2
+%get 3
+> ** 11 **
+
+[CODE]
+%chants add a-plus-b
+    %lines 3
+        %chants update g-add-next %h1 a-plus-b-1
+        %get 6
+        %lines %h1 g-add
+%chants add a-plus-b-1
+    %lines 3
+        %chants update g-bold-next %h1 a-plus-b-2
+        %get 6
+        %lines %h1 g-bold
+%chants add a-plus-b-2
+    %get 3
+
+[DOCS]
+%chants add a-plus-b-man
+```
+Usage:
+    > 1 2
+    > %lines %h1 a-plus-b
+    ...
+    > ** 3 **
+```
+'''
