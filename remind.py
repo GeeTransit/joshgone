309bade1ae330f58b7be3d1d885f31dc358ae69f
George Zhang <geetransit@gmail.com>
2021-05-02 18:54:08 -0400

    Add more suffixes to remind command

--- remind.py
+++ remind.py
@@ -6,6 +6,17 @@ from discord.ext import commands
 
 class Remind(commands.Cog):
 
+    # Ordered by length so that "7minutes" gets parsed as ("7", "minutes") and
+    # not ("7minute", "s").
+    suffixes = {
+        "seconds": 1,
+        "minutes": 60,
+        "hours": 3600,
+        "s": 1,
+        "m": 60,
+        "h": 3600,
+    }
+
     def __init__(self, bot):
         self.bot = bot
         if not hasattr(bot, "_remind_tasks"):
@@ -23,24 +34,29 @@ class Remind(commands.Cog):
             await ctx.send(arg)
 
     @commands.command()
-    async def remind(self, ctx, name: str, minutes, *, arg):
-        """Sends the message after the specified number of minutes
+    async def remind(self, ctx, name: str, time, *, arg):
+        """Sends the message after the specified amount of time
 
         Set name to - or -- to get a randomized reminder name.
-        Add a 's' or 'seconds' prefix to specify seconds instead of minutes.
+
+        The default unit is minutes, but there are other suffixes to specify
+        the unit:
+            -s / -seconds
+            -m / -minutes
+            -h / -hours
         """
         if name == "-" or name == "--":
             name = f"Reminder-{random.randint(100, 999)}"
         if name in self.tasks:
             raise ValueError(f"{name!r} already in tasks")
-        if minutes.endswith("seconds"):
-            minutes = minutes.removesuffix("seconds")
-            seconds = int(minutes)
-        elif minutes.endswith("s"):
-            minutes = minutes.removesuffix("s")
-            seconds = int(minutes)
+        for suffix, multiplier in self.suffixes.items():
+            if time.endswith(suffix):
+                time = int(time.removesuffix(suffix))
+                break
         else:
-            seconds = int(minutes) * 60
+            multiplier = 60
+            time = int(time)
+        seconds = time * multiplier
         task = asyncio.create_task(self._send_after(ctx, seconds, name, arg))
         self.tasks[name] = task
         await ctx.send(f"Added reminder with name {name!r}")
