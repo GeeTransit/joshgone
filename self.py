ca086d3bc2f80b799a8364a49e35d7ac66ed2a81
George Zhang <geetransit@gmail.com>
2021-05-28 11:15:06 -0400

    Improve self command

--- self.py
+++ self.py
@@ -15,7 +15,7 @@ class Self(commands.Cog):
 
     def on(self):
         self.bot._old_process_commands = self.bot.process_commands
-        async def _process_commands(message):  # self is bot
+        async def _process_commands(message):
             await self.bot.invoke(await self.bot.get_context(message))
         self.bot.process_commands = _process_commands
         self.bot._old_skip_check = self.bot._skip_check
@@ -24,19 +24,27 @@ class Self(commands.Cog):
     @commands.command(name="self", ignore_extra=False, hidden=True)
     @commands.is_owner()
     async def _self(self, ctx, accept_self: bool = None):
+        """Make bot respond to own messages
+
+        Usage:
+            %self        -> check whether bot already accepts own messages
+            %self on     -> make bot accept own messages
+            %self off    -> make bot not accept own messages (default)
+        """
         if accept_self is None:
             state = "on" if self.bot._self_accept else "off"
             await ctx.send(f"Self accept is currently {state}")
             return
+        state = "on" if accept_self else "off"
         if accept_self == self.bot._self_accept:
+            await ctx.send(f"Self accept is already {state}")
             return
         self.bot._self_accept = accept_self
         if accept_self:
             self.on()
-            await ctx.send("Self accept is now on")
         else:
             self.off()
-            await ctx.send("Self accept is now off")
+        await ctx.send(f"Self accept is now {state}")
 
 def setup(bot):
     bot.add_cog(Self(bot))
