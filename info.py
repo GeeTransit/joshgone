d29b455b44e8a0a623bcd0cd10f5c37e5217f29b
George Zhang <geetransit@gmail.com>
2021-07-28 02:35:32 -0400

    Refactor listeners to own cog

---
+++ info.py
@@ -0,0 +1,29 @@
+"""Manages general listeners like on_ready or on_command_error"""
+
+import aiosqlite
+
+from discord.ext import commands
+
+class Info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # Some debug info
+        print(f"JoshGone logged on as {self.bot.user}.")
+        print(f"SQLite version is {aiosqlite.sqlite_version}.")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        # Unpack the error for cleaner error messages
+        if isinstance(error, commands.CommandInvokeError):
+            error = error.__cause__ or error
+        try:
+            await ctx.send(f"Oops, an error occurred: `{error!r}`")
+        except Exception:
+            print(f"Error: {error!r}")
+            raise
+
+def setup(bot):
+    bot.add_cog(Info(bot))
--- joshgone.py
+++ joshgone.py
@@ -1,13 +1,12 @@
 import os
 
-import aiosqlite
 import discord
 from discord.ext import commands
 
 # These extensions are loaded automatically on startup
 LOAD_ON_STARTUP = '''
     admin censor chant music database thicc gee remind split get relay solver
-    sort repl
+    sort repl info
 '''.split()
 
 # We need intents to resolve a name to a Member object
@@ -16,23 +15,6 @@ intents.members = True
 
 # Our prefix is % or @joshgone
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("%"), intents=intents)
-
-@bot.event
-async def on_ready():
-    # Some debug info
-    print(f"JoshGone logged on as {bot.user}.")
-    print(f"SQLite version is {aiosqlite.sqlite_version}.")
-
-@bot.event
-async def on_command_error(ctx, error):
-    # Unpack the error for cleaner error messages
-    if isinstance(error, commands.CommandInvokeError):
-        error = error.__cause__ or error
-    try:
-        await ctx.send(f"Oops, an error occurred: `{error!r}`")
-    except Exception:
-        print(f"Error: {error!r}")
-        raise
 
 # Load extensions
 for module in LOAD_ON_STARTUP:
