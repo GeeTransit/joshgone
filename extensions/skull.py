cb0f2093de33680e3fb79ca3dbeb4c19932a5937
George Zhang <geetransit@gmail.com>
2022-05-05 19:09:24 -0400

    Delete skull GIF after 2 seconds
    Changelog: change::

--- extensions/skull.py
+++ extensions/skull.py
@@ -1,3 +1,3 @@
 def setup(B):
  @B.listen()
- async def on_message(M):"💀"in M.content and await M.channel.send("http://tenor.com/view/10107813")
+ async def on_message(M):"💀"in M.content and await M.channel.send("http://tenor.com/view/10107813",delete_after=2)
